3b1c569684b83b6d9fd6411d2e6026c6833ce690
Predictor Librus-Quattuor <51878699+PredictorLQ@users.noreply.github.com>
2020-09-20 04:36:47 +0700

    Update test.js

--- js/test.js
+++ js/test.js
@@ -13,8 +13,14 @@ function color_point(integer) {
         x: 0 + radius * Math.sin(alpha.toRad()) + "px",
         y: 0 + radius * Math.cos(alpha.toRad()) + "px"
     };
-    $("#csd-dot1").css({ "left": coord.x, "top": coord.y });
+    $("#csd-dot1").css({ "left": coord.x, "top": coord.y, "background": "hsl(" + Number(180 - alpha) + ", 100%, 50%)" });
     $("#csd-sample").css("background", "hsl(" + Number(180 - alpha) + ", 100%, 50%)");
+}
+
+function pieSlicer(val, del, id) {
+    var cirfer = Number(2 * $(id).attr("r") * Math.PI)
+    var pVal = (val / del) * cirfer;
+    $(id).css({ "stroke-dasharray": pVal + " " + cirfer });
 }
 $(document).ready(function() {
     $(".question2").css("display", "none");
@@ -31,9 +37,15 @@ $(document).ready(function() {
             $(".result").css("display", "block");
             flag_count = 1;
             var text_answer = "<table class='w-100 table-answer'>";
+            var nums = [];
             for (var i = 1; i < answer.length; i++) {
-                text_answer += "<tr><td>Вопрос №" + i + "</td><td>" + Math.round(answer[i] / 360 * 100) + "%</td></tr>";
+                let item = Math.round(answer[i] / 360 * 100);
+                nums.push(item);
+                text_answer += "<tr><td>Вопрос №" + i + "</td><td>" + item + "%</td></tr>";
             }
+            var count = nums.reduce((a, b) => (a + b)) / nums.length;
+            pieSlicer(count, 100, "#svg-bac");
+            $(".test-svg-result").text(count);
             text_answer += "</table>"
             $("#answ-res").html(text_answer);
         }
